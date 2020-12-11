9830c6e48dda4391bcf51c69d316057c352f59ad
MaheshBantaram <maheshbantaram@gmail.com>
2020-12-11 17:46:21 +0530

    Changes to add/edit components.

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AddItemComponent } from './add-item/add-item.component';
+import { EditItemComponent } from './edit-item/edit-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsComponent
+    ItemsComponent,
+    AddItemComponent,
+    EditItemComponent
   ],
   imports: [
     BrowserModule,
